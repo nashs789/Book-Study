60b7534befe9d6d9b1bd036745cac6021d9ecdb2
nashs789 <nashs789@naver.com>
2024-03-18 23:51:32 +0900

    feat: added changes to Member entity

--- ORM/자바 ORM 표준 프로그래밍/jpa-self-taught/src/main/java/spring/jpa/jpaselftaught/entity/Member.java
+++ ORM/자바 ORM 표준 프로그래밍/jpa-self-taught/src/main/java/spring/jpa/jpaselftaught/entity/Member.java
@@ -2,21 +2,21 @@ package spring.jpa.jpaselftaught.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
-@Getter
-@Setter
+@Data
 @Builder
+@Entity
+@Table(name = "MEMBER")
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-public class Test {
+public class Member {
 
-    @Id @GeneratedValue
-    private Long id;
+    @Id
+    @Column(name = "ID")
+    private String id;
 
-    @Column(name = "name")
-    private String name;
+    private int age;
 }
